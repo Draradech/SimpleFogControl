3de5c3c1da67bcbb4bb7e1b0e2fb98848a15b37c
Manuel Kasten <kasten.m@gmx.de>
2022-05-19 19:49:01 +0200

    update to 1.19 pre 1

--- src/main/java/de/draradech/simplefog/mixin/FogRendererMixin.java
+++ src/main/java/de/draradech/simplefog/mixin/FogRendererMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.FogRenderer;
 import net.minecraft.client.renderer.FogRenderer.FogMode;
 import net.minecraft.core.Holder;
+import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -22,13 +23,18 @@ import de.draradech.simplefog.SimpleFogMain;
 
 @Mixin(FogRenderer.class)
 public class FogRendererMixin {
-    @Inject(at = @At("TAIL"), method = "setupFog(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/FogRenderer$FogMode;FZ)V")
-    private static void afterSetupFog(Camera camera, FogMode fogMode, float viewDistance, boolean thickFog, CallbackInfo info) {
+    @Inject(at = @At("TAIL"), method = "setupFog(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/FogRenderer$FogMode;FZF)V")
+    private static void afterSetupFog(Camera camera, FogMode fogMode, float viewDistance, boolean thickFog, float partialTick, CallbackInfo info) {
         FogType fogType = camera.getFluidInCamera();
         Entity entity = camera.getEntity();
+        boolean mobEffect = (  (entity instanceof LivingEntity)
+                            && (  ((LivingEntity)entity).hasEffect(MobEffects.BLINDNESS)
+                               || ((LivingEntity)entity).hasEffect(MobEffects.DARKNESS)
+                               )
+                            ); 
         if (fogType == FogType.LAVA) {
         } else if (fogType == FogType.POWDER_SNOW) {
-        } else if (entity instanceof LivingEntity && ((LivingEntity)entity).hasEffect(MobEffects.BLINDNESS)) {
+        } else if (mobEffect) {
         } else if (fogType == FogType.WATER) {
             if(SimpleFogMain.config.waterToggle) overrideWaterFog(viewDistance, entity);
         } else if (thickFog) {
@@ -39,7 +45,6 @@ public class FogRendererMixin {
         }
     }
 
-    @SuppressWarnings("deprecation")
     private static void overrideWaterFog(float viewDistance, Entity entity) {
         float fogStart, fogEnd;
         fogStart = viewDistance * SimpleFogMain.config.waterStart * 0.01f;
@@ -47,7 +52,7 @@ public class FogRendererMixin {
         if (entity instanceof LocalPlayer) {
             LocalPlayer localPlayer = (LocalPlayer)entity;
             Holder<Biome> biomeHolder = localPlayer.level.getBiome(localPlayer.blockPosition());
-            if (Biome.getBiomeCategory(biomeHolder) == Biome.BiomeCategory.SWAMP) {
+            if (biomeHolder.is(BiomeTags.HAS_CLOSER_WATER_FOG)) {
                 fogEnd = viewDistance * SimpleFogMain.config.waterEndSwamp * 0.01f;
             }
             fogEnd *= Math.max(0.25f, localPlayer.getWaterVision());
