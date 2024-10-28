cc57092bc2babddebaf22fe026508fd10b46b060
Manuel Kasten <kasten.m@gmx.de>
2024-10-28 14:33:48 +0100

    update to 1.21.3

--- src/main/java/de/draradech/simplefog/mixin/FogRendererMixin.java
+++ src/main/java/de/draradech/simplefog/mixin/FogRendererMixin.java
@@ -1,9 +1,8 @@
 package de.draradech.simplefog.mixin;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-
 import net.minecraft.client.Camera;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.renderer.FogParameters;
 import net.minecraft.client.renderer.FogRenderer;
 import net.minecraft.client.renderer.FogRenderer.FogMode;
 import net.minecraft.core.Holder;
@@ -14,17 +13,20 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.material.FogType;
 
+import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import de.draradech.simplefog.SimpleFogMain;
 
 @Mixin(FogRenderer.class)
 public class FogRendererMixin {
-    @Inject(at = @At("TAIL"), method = "setupFog(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/FogRenderer$FogMode;FZF)V")
-    private static void afterSetupFog(Camera camera, FogMode fogMode, float viewDistance, boolean thickFog, float partialTick, CallbackInfo info) {
+    @Inject(at = @At("RETURN"), method = "setupFog(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/FogRenderer$FogMode;Lorg/joml/Vector4f;FZF)Lnet/minecraft/client/renderer/FogParameters;", cancellable = true)
+    private static void afterSetupFog(Camera camera, FogMode fogMode, Vector4f fogColor, float viewDistance, boolean thickFog, float partialTick, CallbackInfoReturnable<FogParameters> info) {
+        FogParameters parameters = info.getReturnValue();
+        if (parameters == FogParameters.NO_FOG) return;
         FogType fogType = camera.getFluidInCamera();
         Entity entity = camera.getEntity();
         boolean mobEffect = (  (entity instanceof LivingEntity)
@@ -36,17 +38,18 @@ public class FogRendererMixin {
         } else if (fogType == FogType.POWDER_SNOW) {
         } else if (mobEffect) {
         } else if (fogType == FogType.WATER) {
-            if(SimpleFogMain.config.waterToggle) overrideWaterFog(viewDistance, entity);
+            if(SimpleFogMain.config.waterToggle) overrideWaterFog(viewDistance, entity, info);
         } else if (thickFog) {
-            if(SimpleFogMain.config.netherToggle) overrideNetherFog(viewDistance);
+            if(SimpleFogMain.config.netherToggle) overrideNetherFog(viewDistance, info);
         } else if (fogMode == FogMode.FOG_SKY) {
         } else {
-            if(SimpleFogMain.config.terrainToggle) overrideTerrainFog(viewDistance, fogMode);
+            if(SimpleFogMain.config.terrainToggle) overrideTerrainFog(viewDistance, info);
         }
     }
 
-    private static void overrideWaterFog(float viewDistance, Entity entity) {
+    private static void overrideWaterFog(float viewDistance, Entity entity, CallbackInfoReturnable<FogParameters> info) {
         float fogStart, fogEnd;
+        FogParameters parameters = info.getReturnValue();
         fogStart = viewDistance * SimpleFogMain.config.waterStart * 0.01f;
         fogEnd = viewDistance * SimpleFogMain.config.waterEnd * 0.01f;
         if (entity instanceof LocalPlayer) {
@@ -57,23 +60,22 @@ public class FogRendererMixin {
             }
             fogEnd *= Math.max(0.25f, localPlayer.getWaterVision());
         }
-        RenderSystem.setShaderFogStart(fogStart);
-        RenderSystem.setShaderFogEnd(fogEnd);
+        info.setReturnValue(new FogParameters(fogStart, fogEnd, parameters.shape(), parameters.red(), parameters.green(), parameters.blue(), parameters.alpha()));
     }
 
-    private static void overrideNetherFog(float viewDistance) {
+    private static void overrideNetherFog(float viewDistance, CallbackInfoReturnable<FogParameters> info) {
         float fogStart, fogEnd;
+        FogParameters parameters = info.getReturnValue();
         fogStart = viewDistance * SimpleFogMain.config.netherStart * 0.01f;
         fogEnd = viewDistance * SimpleFogMain.config.netherEnd * 0.01f;
-        RenderSystem.setShaderFogStart(fogStart);
-        RenderSystem.setShaderFogEnd(fogEnd);
+        info.setReturnValue(new FogParameters(fogStart, fogEnd, parameters.shape(), parameters.red(), parameters.green(), parameters.blue(), parameters.alpha()));
     }
 
-    private static void overrideTerrainFog(float viewDistance, FogMode fogMode) {
+    private static void overrideTerrainFog(float viewDistance, CallbackInfoReturnable<FogParameters> info) {
         float fogStart, fogEnd;
+        FogParameters parameters = info.getReturnValue();
         fogStart = viewDistance * SimpleFogMain.config.terrainStart * 0.01f;
         fogEnd = viewDistance * SimpleFogMain.config.terrainEnd * 0.01f;
-        RenderSystem.setShaderFogStart(fogStart);
-        RenderSystem.setShaderFogEnd(fogEnd);
+        info.setReturnValue(new FogParameters(fogStart, fogEnd, parameters.shape(), parameters.red(), parameters.green(), parameters.blue(), parameters.alpha()));
     }
 }
