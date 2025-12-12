c7cd42f971ddd8ad01205a45846633c576bc7275
orangishcat <fdsnail2@gmail.com>
2025-12-12 08:44:22 -0800

    chore: fix autoconfig

--- neoforge/src/main/java/de/draradech/simplefog/SimpleFogNeoforge.java
+++ neoforge/src/main/java/de/draradech/simplefog/SimpleFogNeoforge.java
@@ -1,24 +1,18 @@
 package de.draradech.simplefog;
 
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.clothconfig2.ClothConfigDemo;
-import net.neoforged.bus.api.IEventBus;
+import me.shedaniel.autoconfig.*;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(SimpleFogNeoforge.MODID)
 public class SimpleFogNeoforge {
     public static final String MODID = "simplefog";
 
-    public static SimpleFogConfig config;
-    
     public SimpleFogNeoforge()
     {
         SimpleFogMain.init();
-        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (container, parent) -> {
-            return AutoConfig.getConfigScreen(SimpleFogConfig.class, parent).get();
-        });
+        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class,() -> (container, parent) ->
+                AutoConfigClient.getConfigScreen(SimpleFogConfig.class, parent).get());
     }
 }
